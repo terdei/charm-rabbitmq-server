terdei/charm-rabbitmq-server

commit a8f7dea190a6e698d7851642fd16b67c5ed4f6fa

Write stats to tmp file before updating data file
This change updates the stats collection script so it writes to a
temporary file before updating the main stats file to ensure that
the update is an atomic operation.

Change-Id: I68d4e3e723ed62fb5e90ba6bcf284c432ba9cd6b
Closes-Bug: #1569143

scripts/collect_rabbitmq_stats.sh
@@ -43,11 +43,13 @@ fi
 if [ ! -d $LOG_DIR ]; then
     mkdir -p $LOG_DIR
 fi
+TMP_DATA_FILE=$(mktemp -p ${DATA_DIR})
 echo "#Vhost Name Messages_ready Messages_unacknowledged Messages Consumers Memory Time" > $DATA_FILE
 /usr/sbin/rabbitmqctl -q list_vhosts | \
 while read VHOST; do
     /usr/sbin/rabbitmqctl -q list_queues -p $VHOST name messages_ready messages_unacknowledged messages consumers memory | \
-    awk "{print \"$VHOST \" \$0 \" $(date +'%s') \"}" >> $DATA_FILE 2>${LOG_DIR}/list_queues.log
+    awk "{print \"$VHOST \" \$0 \" $(date +'%s') \"}" >> ${TMP_DATA_FILE} 2>${LOG_DIR}/list_queues.log
 done
+mv ${TMP_DATA_FILE} ${DATA_FILE}
 echo "mnesia_size: ${MNESIA_DB_SIZE}@$NOW" > $RABBIT_STATS_DATA_FILE
 echo "rss_size: ${RABBIT_RSS}@$NOW" >> $RABBIT_STATS_DATA_FILE

commit 0024aefd135dcc78f669f36f11c655ae439b245d

Fix perms on queue_stats
collect_rabbitmq_stats.sh creates
/var/lib/rabbitmq/data/{}_queue_stats.dat with 600 perms which
cannot be read by the monitoring user. This bug was introduced by
a8f7dea1 which uses mktemp which creates the file with restricted
permissions.

Change-Id: Idf3022bf925dd02b4bb6c979980ec1c6211b6923
Closes-Bug: #1730604

scripts/collect_rabbitmq_stats.sh
@@ -51,5 +51,6 @@ while read VHOST; do
     awk "{print \"$VHOST \" \$0 \" $(date +'%s') \"}" >> ${TMP_DATA_FILE} 2>${LOG_DIR}/list_queues.log
 done
 mv ${TMP_DATA_FILE} ${DATA_FILE}
+chmod 644 ${DATA_FILE}
 echo "mnesia_size: ${MNESIA_DB_SIZE}@$NOW" > $RABBIT_STATS_DATA_FILE
 echo "rss_size: ${RABBIT_RSS}@$NOW" >> $RABBIT_STATS_DATA_FILE